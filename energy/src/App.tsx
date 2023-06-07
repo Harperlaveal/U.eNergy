9f8343b58cd7138f0ce1833b892d5922c96dda76
Gareth McIntosh <gareth.mcintosh11@gmail.com>
2023-06-07 12:52:09 +1200

    Created React app with typescript & installed d3

---
+++ energy/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import * as d3 from "d3";
+
+const App: React.FC = () => {
+  useEffect(() => {
+    // Implement D3.js code here
+  }, []);
+
+  return (
+    <div>
+      <h1>Energy Usage and Consumption Data Visualisation</h1>
+      <div id="chart"></div>
+      {/* Add other components and elements for your web page */}
+    </div>
+  );
+};
+
+export default App;
