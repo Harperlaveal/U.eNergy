967b8e450e0c34893b64559178b075db12a92f00
Ben Sanson <sansonbenj@myvuw.ac.nz>
2023-06-13 14:03:59 +1200

    create first next js page

---
+++ energy-visualisation/src/app/similar-countries/page.tsx
@@ -0,0 +1,13 @@
+// import the react library 
+import React from 'react';
+
+// export a component that has a div with header inside that says countries
+export const SimilarCountriesPage = () => { 
+    return (
+      <div>
+          <h1>Countries</h1>
+      </div>
+    ); 
+};
+
+export default SimilarCountriesPage;
