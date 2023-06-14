15709882d9ffe74ee29c566bf20f5bce0d2aa4d6
kusaymoo <kusaymoo@vuw.ac.nz>
2023-06-14 20:27:04 +1200

    country-page: add play pause button.

--- energy-visualisation/src/app/country/components/timeline.tsx
+++ energy-visualisation/src/app/country/components/timeline.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { EnergyProductionData } from "../interfaces";
 import Slider from "@mui/material/Slider";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
 interface TimelineProps {
   data: EnergyProductionData[];
@@ -9,18 +11,67 @@ interface TimelineProps {
 
 export default function Timeline({ data, onYearChange }: TimelineProps) {
   const [value, setValue] = useState<number>(data[data.length - 1].year);
+  const [index, setIndex] = useState<number>(data.length - 1);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
   const handleChange = (event: Event, newValue: number | number[]) => {
     if (typeof newValue === "number") {
       setValue(newValue);
       onYearChange(newValue);
+      const closestYear = data.reduce((prev, curr) => 
+        Math.abs(curr.year - newValue) < Math.abs(prev.year - newValue) ? curr : prev
+      );
+      const newIndex = data.findIndex(d => d.year === closestYear.year);
+      if (newIndex !== -1) {
+        setIndex(newIndex);
+      }
+      if (isPlaying) {
+        setIsPlaying(false);
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+        }
+      }
+    }
+  };
+
+  const handlePlay = () => {
+    setIsPlaying(!isPlaying);
+    if (!isPlaying) {
+      intervalRef.current = setInterval(() => {
+        setIndex((prevIndex) => {
+          const nextIndex = prevIndex >= data.length - 1 ? 0 : prevIndex + 1;
+          return nextIndex;
+        });
+      }, 1000);
+    } else {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
     }
   };
 
   const totalYears = data[data.length - 1].year - data[0].year;
 
+  useEffect(() => {
+    onYearChange(data[index].year);
+    setValue(data[index].year);
+  }, [index, onYearChange]);
+
   return (
     <div className="mt-8 w-full relative">
+      <button onClick={handlePlay}>
+        {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+      </button>
       <Slider
         value={value}
         onChange={handleChange}
@@ -36,7 +87,7 @@ export default function Timeline({ data, onYearChange }: TimelineProps) {
           "& .MuiSlider-track": {
             transition: "width 0.3s ease-in-out",
             "&:not(.MuiSlider-active)": {
-              transition: "width  0.3s ease-in-out",
+              transition: "width 0.3s ease-in-out",
             },
           },
         }}
