2843e1a4f666b090f4ac1e03c68304aa0f42469e
Gareth McIntosh <gareth.mcintosh11@gmail.com>
2023-06-16 02:58:33 +1200

    implemented plasos timeline

---
+++ energy-visualisation/src/app/aggregate-countries/components/timeline.tsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect, useRef } from "react";
+import { EnergyProductionData } from "../../country/interfaces";
+import Slider from "@mui/material/Slider";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+
+interface TimelineProps {
+  years: number[];
+  onYearChange: (year: number) => void;
+}
+
+export default function Timeline({ years, onYearChange }: TimelineProps) {
+  const [value, setValue] = useState<number>(years[years.length - 1]);
+  const [index, setIndex] = useState<number>(years.length - 1);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    if (typeof newValue === "number") {
+      setValue(newValue);
+      onYearChange(newValue);
+      const closestYear = years.reduce((prev, curr) =>
+        Math.abs(curr - newValue) < Math.abs(prev - newValue)
+          ? curr
+          : prev
+      );
+      const newIndex = years.findIndex((d) => d === closestYear);
+      if (newIndex !== -1) {
+        setIndex(newIndex);
+      }
+      if (isPlaying) {
+        setIsPlaying(false);
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+        }
+      }
+    }
+  };
+
+  const handlePlay = () => {
+    setIsPlaying(!isPlaying);
+    if (!isPlaying) {
+      intervalRef.current = setInterval(() => {
+        setIndex((prevIndex) => {
+          const nextIndex = prevIndex >= years.length - 1 ? 0 : prevIndex + 1;
+          return nextIndex;
+        });
+      }, 1000);
+    } else {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    }
+  };
+
+  const totalYears = years[years.length - 1] - years[0];
+
+  useEffect(() => {
+    onYearChange(years[index]);
+    setValue(years[index]);
+  }, [index, onYearChange, years]);
+
+ return (
+    <div className="mt-8 w-full relative">
+      {/* <LineGraph data={years} selectedYear={value} /> */}
+      <button onClick={handlePlay}>
+        {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+      </button>
+      <Slider
+        value={value}
+        onChange={handleChange}
+        min={parseInt(years[0].toString())}
+        max={parseInt(years[years.length - 1].toString())}
+        sx={{
+          "& .MuiSlider-thumb": {
+            transition: "left 0.3s ease-in-out",
+            "&:not(.MuiSlider-active)": {
+              transition: "left 0.3s ease-in-out",
+            },
+          },
+          "& .MuiSlider-track": {
+            transition: "width 0.3s ease-in-out",
+            "&:not(.MuiSlider-active)": {
+              transition: "width 0.3s ease-in-out",
+            },
+          },
+        }}
+      />
+      <div className="flex justify-between text-xs w-full">
+        {years.map((year, i) => (
+          <button
+            className={`hover: ${
+              year === value ? "text-blue-500" : ""
+            }`}
+            onClick={() => {
+              setIndex(i);
+              setValue(year);
+              onYearChange(year);
+            }}
+            key={year}
+            style={{
+              position: "absolute",
+              left: `${((year - years[0]) / totalYears) * 100}%`,
+              transform: "translateX(-50%)",
+            }}
+          >
+            {year}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
--- energy-visualisation/src/app/aggregate-countries/page.tsx
+++ energy-visualisation/src/app/aggregate-countries/page.tsx
@@ -3,16 +3,10 @@
 import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 import BarChart from './components/barchart';
-import Timeline from '../country/components/timeline';
+import Timeline from './components/timeline';
 import { EnergyProductionData } from '../country/interfaces';
 import { loadCSVData } from '../country/utils';
 import { createCountryData } from '../country/utils';
-
-interface Country {
-  country: string;
-  amount: number;
-  id: string;
-}
 
 const productionMethods: string[] = ["Hydro", "Nuclear", "Solar", "Wind", "Other renewables", "Natural gas", "Coal", "Oil"];
 
@@ -23,7 +17,7 @@ const SimilarCountriesPage = () => {
     [country: string]: EnergyProductionData[];
   }>({});
   const [countryCount, setCountryCount] = useState<number>(10);
-  const [years, setYears] = useState<string[]>(['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2020', '2021', '2022']);
+  const [years, setYears] = useState<number[]>([2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020, 2021, 2022]);
   const [selectedYear, setSelectedYear] = useState<number>(2020);
   const [countryList, setCountryList] = useState<string[]>([]);
 
@@ -57,10 +51,10 @@ const SimilarCountriesPage = () => {
         year={selectedYear}
         setSelectedCountry={() => { }}
       />
-      {/* <Timeline
-        data={countryData[countryName]}
+      <Timeline
+        years={years}
         onYearChange={handleYearChange}
-      /> */}
+      />
     </div>
   );
 };
